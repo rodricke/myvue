14a0f4742dc59cdbe94da3d1ab7263b4924ed263
unknown <rodrick@ymail.com>
2018-07-29 21:58:00 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <Skills/>
+    <input ref="autocomplete" 
+        placeholder="Search" 
+        class="search-location"
+        onfocus="value = ''" 
+        type="text" />
+  </div>
+</template>
+
+<script>
+import Skills from './components/Skills.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Skills
+  }
+  ,   mounted() {
+     var input = document.getElementById('searchTextField');
+  new google.maps.places.Autocomplete(input);
+  }
+}
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+
+.search-location {
+  display: block;
+  width: 60vw;
+  margin: 0 auto;
+  margin-top: 5vw;
+  font-size: 20px;
+  font-weight: 400;
+  outline: none;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div class="holder">
+          <input type="text" placeholder="Enter a skill you have.." v-model="skill">
+    {{ skill }}
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
+      </ul>
+      <p>These are the skills that you possess.</p>
+    </div>
+  </div>
+</template>
+
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      name: 'Coursetro',
+      
+
+      skill: '',
+      skills: [
+        {"skill":"Vue.js"},
+        {"skill":"Frontend Developer"}
+      ],
+      showAlert: true,
+       btnState: true
+    }
+         
+  },
+  methods : {
+      addSkill(){
+          this.skills.push({skill: this.skill});
+          this.skill = '';
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="../css/Skills.css" scoped>
+
+</style>
